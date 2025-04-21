b485fd7afcd771e388044375c7c6f342e8d8e1ef
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2025-04-21 02:21:04 -0400

    made some assets

---
+++ src/game/globals.lua
@@ -0,0 +1,7 @@
+gameWidth = 320
+gameHeight = 240
+gameState = "start" 
+debugFlag = false 
+windowWidth, windowHeight = love.graphics.getDimensions()
+
+--stagemanager = require "src.game.stages.stagemanagerinstance"
